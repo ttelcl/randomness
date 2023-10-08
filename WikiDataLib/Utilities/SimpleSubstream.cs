d6098b802df195ed9c373c39a542465017e49be2
ttelcl <9429819+ttelcl@users.noreply.github.com>
2023-10-08 10:17:37 +0300

    SimpleSubstream: implemented read logic (and overloads); removed leftover traces of seek support.

--- WikiDataLib/Utilities/SimpleSubstream.cs
+++ WikiDataLib/Utilities/SimpleSubstream.cs
@@ -14,45 +14,41 @@ using System.Threading.Tasks;
 namespace WikiDataLib.Utilities;
 
 /// <summary>
-/// Exposes a part of another stream
+/// Exposes a view on a slice of a host stream as a separate stream.
+/// This stream is read-only, does not support seeking, and assumes
+/// that the host stream is not used except via this SimpleSubstream
+/// while this stream is "alive"
 /// </summary>
 public class SimpleSubstream: Stream
 {
+  private readonly long _length;
+  private long _position;
+  private bool _disposed;
 
   /// <summary>
-  /// Create a new ReadOnlySubStream, exposing a slice of <paramref name="length"/>
-  /// bytes starting at the host stream's current offset
+  /// Create a new ReadOnlySubStream, exposing the next <paramref name="length"/>
+  /// bytes of the host stream as a read-only nonseekable stream
   /// </summary>
   /// <param name="hostStream">
   /// The host stream from which a part is exposed by this ReadOnlySubstream
   /// Closing this ReadOnlySubstream does not close the host stream.
   /// </param>
   /// <param name="length">
-  /// The length of the stream to expose.
+  /// The length of the stream slice to expose.
   /// </param>
   public SimpleSubstream(Stream hostStream, long length)
   {
-    var offset = hostStream.Position;
     HostStream = hostStream;
+    _length = length;
+    _position = 0;
     if(!hostStream.CanRead)
     {
       throw new ArgumentOutOfRangeException(nameof(hostStream), "Expecting a readable host stream");
     }
-    HostOffset = offset;
-    Size = length;
     if(length < 0)
     {
       throw new ArgumentOutOfRangeException(nameof(length), "length cannot be negative");
     }
-    if(offset < 0)
-    {
-      throw new ArgumentOutOfRangeException(nameof(offset), "offset cannot be negative");
-    }
-    if(length + offset > hostStream.Length)
-    {
-      throw new ArgumentException("The end of the substream cannot be beyond the end of the host stream");
-    }
-    ClampHost();
   }
 
   /// <summary>
@@ -60,96 +56,138 @@ public class SimpleSubstream: Stream
   /// </summary>
   public Stream HostStream { get; init; }
 
-  /// <summary>
-  /// The offset of this ReadOnlySubstream in the host stream
-  /// </summary>
-  public long HostOffset { get; init; }
-
-  /// <summary>
-  /// The immutable length of this ReadOnlySubStream
-  /// </summary>
-  public long Size { get; init; }
-
   /// <inheritdoc/>
   public override bool CanRead { get => true; }
 
   /// <inheritdoc/>
-  public override bool CanSeek { get => HostStream.CanSeek; }
+  public override bool CanSeek { get => false; }
 
   /// <inheritdoc/>
   public override bool CanWrite { get => false; }
 
   /// <inheritdoc/>
-  public override long Length { get => Size; }
+  public override long Length { get => _length; }
 
-  /// <inheritdoc/>
+  /// <summary>
+  /// The number of bytes remaining to be read
+  /// </summary>
+  public long Remaining { get => _length - _position; }
+
+  /// <summary>
+  /// Get the virtual position (as derived from tracking reads).
+  /// This class does not support seeking, so an attempt to set this
+  /// value will throw <see cref="NotSupportedException"/>
+  /// </summary>
   public override long Position {
-    get => HostStream.Position - HostOffset;
-    set {
-      if(value < 0)
-      {
-        value = 0;
-      }
-      if(value > Size)
-      {
-        value = Size;
-      }
-      HostStream.Position = value + HostOffset;
-    }
+    get => !_disposed ? _position : throw new ObjectDisposedException("SimpleSubstream");
+    set => throw new NotSupportedException("SimpleSubstream does not support seeking");
   }
 
-  /// <inheritdoc/>
+  /// <summary>
+  /// Throws <see cref="NotSupportedException"/>
+  /// </summary>
+  /// <exception cref="NotSupportedException">Always thrown</exception>
   public override long Seek(long offset, SeekOrigin origin)
   {
-    switch(origin)
-    {
-      case SeekOrigin.Begin:
-        Position = offset; 
-        break;
-      case SeekOrigin.Current:
-        Position += offset;
-        break;
-      case SeekOrigin.End:
-        Position = Size + offset;
-        break;
-    }
-    return Position;
+    throw new NotSupportedException("SimpleSubstream does not support seeking");
   }
 
   /// <inheritdoc/>
   public override void Flush()
   {
+    // allow calls even if disposed
     HostStream.Flush();
   }
 
   /// <inheritdoc/>
   public override int Read(byte[] buffer, int offset, int count)
   {
-    throw new NotImplementedException();
+    ThrowIfDisposed();
+    if(count > Remaining)
+    {
+      count = (int)Remaining;
+    }
+    if(count <= 0)
+    {
+      return 0;
+    }
+    var n = HostStream.Read(buffer, offset, count);
+    if(n == 0)
+    {
+      throw new EndOfStreamException("Unexpected end of host stream");
+    }
+    _position += n;
+    return n;
   }
 
   /// <inheritdoc/>
-  public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+  public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
   {
-    throw new NotImplementedException();
+    ThrowIfDisposed();
+    if(count > Remaining)
+    {
+      count = (int)Remaining;
+    }
+    if(count <= 0)
+    {
+      return 0;
+    }
+    var n = await HostStream.ReadAsync(buffer, offset, count, cancellationToken);
+    if(n == 0)
+    {
+      throw new EndOfStreamException("Unexpected end of host stream");
+    }
+    _position += n;
+    return n;
   }
 
   /// <inheritdoc/>
   public override int Read(Span<byte> buffer)
   {
-    throw new NotImplementedException();
+    ThrowIfDisposed();
+    var slice = buffer.Length > Remaining ? buffer[..(int)Remaining] : buffer;
+    if(slice.Length == 0)
+    {
+      return 0;
+    }
+    var n = HostStream.Read(slice);
+    if(n == 0)
+    {
+      throw new EndOfStreamException("Unexpected end of host stream");
+    }
+    _position += n;
+    return n;
   }
 
   /// <inheritdoc/>
-  public override ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
+  public override async ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
   {
-    throw new NotImplementedException();
+    ThrowIfDisposed();
+    var slice = buffer.Length > Remaining ? buffer[..(int)Remaining] : buffer;
+    var n = await HostStream.ReadAsync(slice, cancellationToken);
+    if(n == 0)
+    {
+      throw new EndOfStreamException("Unexpected end of host stream");
+    }
+    _position += n;
+    return n;
   }
 
   /// <inheritdoc/>
   public override int ReadByte()
   {
-    throw new NotImplementedException();
+    ThrowIfDisposed();
+    if(Remaining <= 0)
+    {
+      return -1;
+    }
+    var value = HostStream.ReadByte();
+    if(value < 0)
+    {
+      throw new EndOfStreamException("Unexpected end of host stream");
+    }
+    _position++;
+    return value;
   }
 
   /// <summary>
@@ -168,21 +206,20 @@ public class SimpleSubstream: Stream
     throw new NotSupportedException("This implementation is read-only");
   }
 
-  private void ClampHost()
+  /// <summary>
+  /// Clean up. In this case this just sets an internal flag that the stream is disposed
+  /// </summary>
+  protected override void Dispose(bool disposing)
   {
-    HostStream.Position = ClampedPosition(HostStream.Position-HostOffset) + HostOffset;
+    base.Dispose(disposing);
+    _disposed = true;
   }
 
-  private long ClampedPosition(long position)
+  private void ThrowIfDisposed()
   {
-    if(position < 0)
+    if(_disposed)
     {
-      return 0L;
+      throw new ObjectDisposedException("SimpleSubstream");
     }
-    if(position > Size)
-    {
-      return Size;
-    }
-    return position;
   }
 }
