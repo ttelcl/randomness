8f308ad9d50f9350fb192f5309c6f6a59acccc30
ttelcl <9429819+ttelcl@users.noreply.github.com>
2025-06-14 17:19:04 +0300

    Added 'random wordlet'

---
+++ RandomUtilities/Picking/WordletPicker.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * (c) 2025  ttelcl / ttelcl
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RandomUtilities.ByteSources;
+
+namespace RandomUtilities.Picking;
+
+/// <summary>
+/// A specialized <see cref="PickSequence{T}"/> that
+/// returns wordlets concatenated from tiny text fragments
+/// (possibly single-character strings)
+/// </summary>
+public class WordletPicker: PickSequence<string>
+{
+  /// <summary>
+  /// Create a new WordletPicker
+  /// </summary>
+  public WordletPicker(
+    IEnumerable<IEnumerable<string>> fragmentSequence)
+    : base(fragmentSequence)
+  {
+  }
+
+  /// <summary>
+  /// Pick a random wordlet constructed by concatenating
+  /// random picks for each fragment
+  /// </summary>
+  public string PickWordlet(
+    bool capitalize,
+    BitSource randomSource)
+  {
+    var fragments = Pick(randomSource);
+    var wordlet = String.Concat(fragments);
+    if(capitalize)
+    {
+      wordlet = Char.ToUpper(wordlet[0]).ToString() + wordlet[1..];
+    }
+    return wordlet;
+  }
+
+  /// <summary>
+  /// Example WordletPicker 1.
+  /// </summary>
+  public static WordletPicker Instance1 { get; } =
+    new WordletPicker([
+      ["b", "d", "k", "p", "t", "v", ""],
+      ["l", "r", "s", "w"],
+      ["a", "ai", "e", "ee", "i", "o", "oo", "u"],
+      ["l", "r", "s", "w", "f", "ch", "j", "m", "n"],
+      ["", "k", "p", "t"],
+    ]);
+
+  /// <summary>
+  /// Example WordletPicker 2.
+  /// </summary>
+  public static WordletPicker Instance2 { get; } =
+    new WordletPicker([
+      ["b", "d", "k", "p", "t", "v", ""],
+      ["l", "r", "s", "w"],
+      ["a", "ai", "e", "ee", "i", "o", "oo", "u"],
+      ["l", "r", "s", "w", "f", "ch", "j", "m", "n"],
+      ["a", "ai", "e", "ee", "i", "o", "oo", "u", ""],
+      ["", "k", "p", "t"],
+      ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    ]);
+}
---
+++ random/AppWordlets.fs
@@ -0,0 +1,74 @@
+﻿module AppWordlets
+
+open System
+open System.Collections.Generic
+open System.Text
+
+open RandomUtilities.ByteSources
+open RandomUtilities.Picking
+
+open ColorPrint
+open CommonTools
+
+type private Options = {
+  Repeat: int
+  TargetBits: int
+  Template: WordletPicker option
+}
+
+let private runWordlets o =
+  let template = o.Template.Value
+  if template.TotalInformationBits < 1.0 then
+    cp $"\frInvalid template\f0 (low on information, probably empty: {template.TotalInformationBits} bits)"
+    1
+  else
+    cp $"\fgBits per segment = \fb{template.TotalInformationBits:F2}\f0."
+    let randomness = ByteSource.Random().Buffered().ToBitSource()
+    for _ in 1 .. o.Repeat do
+      cpx "  "
+      let mutable pendingBits = float(o.TargetBits)
+      let mutable prefix = ""
+      while pendingBits > 0.0 do
+        let wordlet = template.PickWordlet(true, randomness)
+        pendingBits <- pendingBits - template.TotalInformationBits
+        cpx $"{prefix}{wordlet}"
+        prefix <- "-"
+      cp ""
+    0
+
+let run args =
+  let rec parseMore o args =
+    match args with
+    | "-v" :: rest ->
+      verbose <- true
+      rest |> parseMore o
+    | "-h" :: _ ->
+      None
+    | "-n" :: countText :: rest ->
+      let n = countText |> Int32.Parse
+      rest |> parseMore {o with Repeat = n}
+    | "-bits" :: bitText :: rest ->
+      let bits = bitText |> Int32.Parse
+      rest |> parseMore {o with TargetBits = bits}
+    | [] ->
+      match o.Template with
+      | Some(t) ->
+        o |> Some
+      | None ->
+        cp "\foNo template specified"
+        None
+    | x :: _ ->
+      cp $"\frUnrecognized argument \f0'\fo{x}\f0'"
+      None
+  let oo = args |> parseMore {
+    Repeat = 1
+    TargetBits = 128
+    Template = WordletPicker.Instance2 |> Some
+  }
+  match oo with
+  | None ->
+    Usage.usage "wordlets"
+    1
+  | Some(o) ->
+    o |> runWordlets
+
